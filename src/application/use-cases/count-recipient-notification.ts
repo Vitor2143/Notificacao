c716d5dbc7728f817e3af4b3f9fd95170bcf5718
@vitoraugustobbarros <vitoraugustobbarros@gmail.com>
2022-12-19 19:28:14 -0300

    Projeto Finalizado

---
+++ src/application/use-cases/count-recipient-notification.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common'
+import { NotificationsRepository } from "../repositories/notifications-repository";
+
+
+
+interface CountRecipientNotificationRequest {
+    recipientId: string;
+}
+
+interface CountRecipientNotificationResponse {
+    count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+    constructor(
+        private notificationsRepository: NotificationsRepository) { }
+
+    async execute(
+        request: CountRecipientNotificationRequest,
+    ): Promise<CountRecipientNotificationResponse> {
+        const { recipientId } = request;
+
+        const count = await this.notificationsRepository.countManyByRecipientId(
+            recipientId
+        );
+        return {
+            count,
+        };
+
+    }
+}
